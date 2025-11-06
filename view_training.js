0e02133677cf6c67f62f7b8124e52cbbfc5d9a80
mc250132689 <mc250132689@student.unitar.my>
2025-11-06 22:00:39 +0800

    Update view_training.js

--- view_training.js
+++ view_training.js
@@ -1,1 +1,21 @@
-console.log("View Training page loaded");
+const trainingTableBody = document.getElementById("training-table-body");
+
+async function loadTrainingData() {
+    const response = await fetch("/get-training-data");
+    const data = await response.json();
+    trainingTableBody.innerHTML = "";
+
+    data.training_data.forEach(item => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.question}</td>
+            <td>${item.answer}</td>
+            <td>${item.lang}</td>
+            <td>${item.created_at || ""}</td>
+        `;
+        trainingTableBody.appendChild(tr);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", loadTrainingData);
