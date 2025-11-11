41861a12503169466f2d0283a482160ac0f13441
mc250132689 <mc250132689@student.unitar.my>
2025-11-11 10:13:25 +0800

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -5,34 +5,37 @@ const status = document.getElementById("feedback-status");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   status.textContent = "Submitting...";
+
   const payload = {
     name: document.getElementById("name").value.trim(),
     student_id: document.getElementById("student_id").value.trim(),
     program: document.getElementById("program").value.trim(),
-    q4: document.getElementById("q4").value.trim(),
-    q5: document.getElementById("q5").value.trim(),
-    q6: document.getElementById("q6").value.trim(),
-    q7: document.getElementById("q7").value.trim(),
-    q8: document.getElementById("q8").value.trim(),
-    q9: document.getElementById("q9").value.trim(),
-    q10: document.getElementById("q10").value.trim(),
-    q11: document.getElementById("q11").value.trim(),
-    q12: document.getElementById("q12").value.trim(),
-    q13: document.getElementById("q13").value.trim(),
-    q14: document.getElementById("q14").value.trim(),
-    comments: document.getElementById("q14").value.trim()
+    q4: Number(document.getElementById("q4").value),
+    q5: Number(document.getElementById("q5").value),
+    q6: Number(document.getElementById("q6").value),
+    q7: Number(document.getElementById("q7").value),
+    q8: Number(document.getElementById("q8").value),
+    q9: Number(document.getElementById("q9").value),
+    q10: Number(document.getElementById("q10").value),
+    q11: Number(document.getElementById("q11").value),
+    q12: Number(document.getElementById("q12").value),
+    q13: Number(document.getElementById("q13").value),
+    q14: Number(document.getElementById("q14").value),
+    comments: document.getElementById("comments").value.trim()
   };
+
   try {
     const res = await fetch(FEEDBACK_API, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
     });
+
     const data = await res.json();
     status.textContent = data.message || "Submitted. Thank you!";
     form.reset();
+
   } catch (err) {
-    console.error(err);
-    status.textContent = "Error submitting feedback. Please try later.";
+    status.textContent = "Error submitting feedback. Please try again later.";
   }
 });
